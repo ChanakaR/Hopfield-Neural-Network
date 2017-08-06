faec3cfe66145d3800be178af947ae04f2c64a6a
Chanakar <bmcrathnayaka@gmail.com>
2017-08-06 07:33:34 +0530

    added bipolar function

--- src/Hopfield.java
+++ src/Hopfield.java
@@ -3,4 +3,23 @@
  */
 public class Hopfield {
 
+    private int[] bipolar_version;
+
+
+    public void train(){
+
+    }
+
+    private int[] get_bipolar_representation(int[] input,int n){
+        int[] bipolar = new int[n];
+        for (int i=0;i<n;i++){
+            if(input[i] == 0){
+                bipolar[i] = -1;
+            }else{
+                bipolar[i] = input[i];
+            }
+        }
+        return bipolar;
+    }
+
 }
